920bb5c9cffa09377c708448bd8c6fd832f8ad29
Richardrafael <richard.rafael.soares@gmail.com>
2023-09-10 23:29:45 -0300

    fix: rederização das imagens

--- src/components/menu/OpenMenu.vue
+++ src/components/menu/OpenMenu.vue
@@ -1,17 +1,25 @@
 <template >
+  <RouterView/>
     <div class="bg-[#CCFF00] h-screen w-[5rem] fixed justify-start flex-col gap-6 p-2 items-center flex ">
-       <BtnMenu caminho="/src/assets/logo.svg"/> 
-       <BtnMenu caminho="/src/assets/Bell.svg"/> 
-       <BtnMenu caminho="/src/assets/Home.svg"/> 
-       <BtnMenu caminho="/src/assets/Sum.svg"/> 
+      <RouterLink to="/">
+        <BtnMenu caminho="/assets/logo.svg"/> 
+      </RouterLink>
+       <BtnMenu caminho="/assets/Bell.svg"/> 
+       <RouterLink to="/">
+         <BtnMenu caminho="/assets/Home.svg"/> 
+       </RouterLink>
+       <RouterLink to='/Cadastro_Cargo'>
+         <BtnMenu caminho="/assets/Sum.svg"/> 
+       </RouterLink>
       <div class="flex absolute bottom-2 mb-[0.7rem]  cursor-pointer  flex-row gap-5">
         <img alt="Vue logo" 
     class="logo" 
-    src="../../assets/Export.svg"
+    src="/assets/Export.svg"
     width="40" height="40" />
       </div>
     </div>
 </template>
 <script lang="ts" setup>
 import BtnMenu from "@/components/menu/BtnMenu.vue";
+import { RouterLink, RouterView } from 'vue-router'
 </script>
--- src/views/Cadastro_Cargo.vue
+++ src/views/Cadastro_Cargo.vue
@@ -6,15 +6,16 @@ const name = ref('')
 const level = ref('')
 const desc = ref('')
 const erro = ref('')
-// let edit = false
-const editable = ref()
+const isDisabled = ref(true);
+const Play = ref(false);
+
 
 
 async function cadatrar() {
   try{
     await axios.post("https://jsonplaceholder.typicode.com/posts" ,{
     nome_cargo: name.value, 
-    nivel_cargo :level.value,
+    nivel_cargo : level.value,
     descricao_cargo : desc.value
   });
   console.log(`${name.value} + ${level.value} + ${desc.value}`)
@@ -24,8 +25,12 @@ async function cadatrar() {
   console.log("passei")
 }
 
-const editable_desc = () => {
+const habilitarInput = () => {
+  isDisabled.value = !isDisabled.value;
+};
 
+const played = () =>{
+  Play.value = true
 }
 
 </script>
@@ -46,7 +51,8 @@ const editable_desc = () => {
       </span>
       <input  
      v-model="name" id="name"
-      class="bg-[#2A753D] w-full h-11 p-2 shadow-md outline-none rounded-xl text-[#FFF]">   
+     placeholder="Nome do Cargo"
+      class="bg-[#2A753D] w-full h-11 p-2 pt-2 shadow-md outline-none rounded-xl text-[#FFF]">   
     </div>
     <div class="w-1/3 flex flex-col ">
       <span class="bg-[#FFD600] w-[7rem] font-semibold top-[7.5rem] shadow-md rounded-lg ml-4 absolute  text-center">
@@ -54,7 +60,8 @@ const editable_desc = () => {
       </span>
       <input 
       v-model="level" id="level"
-       class="bg-[#2A753D] w-full h-11 p-2 shadow-md outline-none rounded-xl text-[#FFF]">   
+      placeholder="Nome do Cargo"
+       class="bg-[#2A753D] w-full h-11 p-2 pt-2 shadow-md outline-none rounded-xl text-[#FFF]">   
     </div>
        </div>
           <div class="w-full rounded-2xl px-10 py-7 h-[45vh] flex flex-col shadow-md text-white bg-[#2A753D]">
@@ -65,21 +72,24 @@ const editable_desc = () => {
               <textarea 
                 rows="4" 
                 v-model="desc" id="desc"
+                :disabled="isDisabled"
                 placeholder="sua descrição será gerada"
                 class="w-5/6 h-[75%] bg-[#2A753D] p-4 focus:outline-none flex resize-none shadow-xl justify-start rounded-xl">
               </textarea>
-             <div class="bg-black rounded-full shadow-md p-5 flex justify-center  h-[3.7rem]">
-               <img src="../assets/play.svg" width="20" height="20" alt="" srcset="">
+             <div @click="played" class="bg-black cursor-pointer rounded-full shadow-md p-5 flex justify-center  h-[3.7rem]">
+               <img src="/assets/play.svg" width="20" height="20" alt="" srcset="">
              </div>
             </div>
             <div 
-            @click="editable_desc"
+            v-if="Play"
+            @click="habilitarInput"
             class="bg-[#FFD600] text-black p-[0.3rem] w-32 shadow-md rounded-xl flex justify-center">
-                <p class="font-bold text-base" >Aprimorar</p>
+                <p v-if="isDisabled" class="font-bold text-base" >Aprimorar</p>
+                <p v-else class="font-bold text-base" >Voltar</p>
             </div>
           </div>
           <div class="w-full flex justify-center text-[#fff] ">
-              <button class="bg-[#263001] w-[15rem] rounded-xl" @click="cadatrar" type="submit" value="Cadastrar" >
+              <button v-if="Play" class="bg-[#263001] w-[15rem] rounded-xl" @click="cadatrar" type="submit" value="Cadastrar" >
               <p class="text-lg font-bold p-1">
                 Cadastrar
               </p>
