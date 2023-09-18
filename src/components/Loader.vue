3e2d8de99216a0ec5b52837bc1545d540996a5d9
Richardrafael <richard.rafael.soares@gmail.com>
2023-09-18 14:18:01 -0300

    feat: add loader and bugfix campo

---
+++ src/components/Loader.vue
@@ -0,0 +1,6 @@
+<template >
+        <div class="border-t-4 border-blue-50 border-solid rounded-full w-16 h-16 animate-spin"></div>
+</template>
+<script setup lang="ts">
+
+</script>
--- src/views/Cadastro_Cargo.vue
+++ src/views/Cadastro_Cargo.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import OpenMenu from '@/components/menu/OpenMenu.vue'
+import Loader from '@/components/Loader.vue'
 import Alert from '@/components/alert/Alert.vue'
 import axios from 'axios'
 import { ref } from 'vue'
@@ -12,6 +13,7 @@ const erro = ref('')
 const isDisabled = ref(true)
 const isDone = ref(false)
 const Play = ref(false)
+const loading = ref(false)
 
 async function cadatrar() {
   try {
@@ -33,22 +35,19 @@ async function cadatrar() {
 }
 
 async function getResponseChatgpt(name: String, level: String) {
-  console.log('carregando')
+  loading.value = true
   try {
     await axios.get(`https://jsonplaceholder.typicode.com/chatgp/${name} ${level}`, {})
     played()
   } catch (err) {
     erro.value = (err as Error).message
   }
-  // played()
+  loading.value = false
 }
 
 const habilitarInput = () => {
   isDisabled.value = !isDisabled.value
 }
-// const success = () => {
-
-// }
 
 const played = () => {
   Play.value = true
@@ -69,12 +68,12 @@ const played = () => {
       </h1>
       <p class="text-red-700 fixed">{{ erro }}</p>
       <div
-        class="xl:w-[88vw] w-[90%] flex flex-col gap-10 p-4 mt-[3rem] h-full bg-[#1DEEA3] shadow-md bg-opacity-30 rounded-2xl"
+        class="xl:w-[88vw] w-[90%] flex flex-col gap-10 p-4 mt-[3rem] bg-[#1DEEA3] shadow-md bg-opacity-30 rounded-2xl"
       >
         <div class="w-full flex flex-col gap-5 xl:gap-0 xl:flex-row justify-between">
-          <div class="xl:w-1/3 w-full flex flex-col">
+          <div class="xl:w-1/3 w-full flex flex-col relative">
             <span
-              class="bg-[#FFD600] w-[7rem] font-semibold shadow-md rounded-lg ml-4 absolute text-center"
+              class="bg-[#FFD600] w-[7rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10"
             >
               Nome
             </span>
@@ -82,63 +81,106 @@ const played = () => {
               v-model="name"
               id="name"
               placeholder="Nome do Cargo"
-              class="bg-[#2A753D] w-full h-11 p-2 pt-2 shadow-md outline-none rounded-xl text-[#FFF]"
+              class="bg-[#2b8341e2] w-full h-11 p-2 pt-2 shadow-md outline-none rounded-xl text-[#FFF] relative z-0"
             />
           </div>
-          <div class="xl:w-1/3 w-full flex flex-col">
+          <div class="xl:w-1/3 w-full flex flex-col relative">
             <span
-              class="bg-[#FFD600] w-[7rem] font-semibold  shadow-md rounded-lg ml-4 absolute text-center"
+              class="bg-[#FFD600] w-[7rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10"
             >
               Nível
             </span>
             <input
               v-model="level"
               id="level"
-              placeholder="Nome do Cargo"
-              class="bg-[#2A753D] w-full h-11 p-2 pt-2 shadow-md outline-none rounded-xl text-[#FFF]"
+              placeholder="Nível do Cargo"
+              class="bg-[#2b8341e2] w-full h-11 p-2 pt-2 shadow-md outline-none rounded-xl text-[#FFF] relative z-0"
             />
           </div>
         </div>
-        <div 
-          class="w-full rounded-2xl px-10 py-7 h-[100vh] overflow-y-visible  flex flex-col shadow-md text-white bg-[#2A753D]"
+        <div
+          class="w-full rounded-2xl relative px-10 py-7 h-auto overflow-y-visible flex flex-col shadow-md text-white bg-[#2b8341e2]"
         >
           <span
-            class="bg-[#FFD600] w-[7rem]  shadow-md rounded-lg text-black font-semibold absolute text-center"
+            class="bg-[#FFD600] top-[-0.7rem] w-[7rem] shadow-md rounded-lg text-black font-semibold absolute text-center"
           >
             CHA
           </span>
 
           <div class="h-full flex flex-col gap-8 justify-between">
             <textarea
+              v-if="!loading"
               rows="4"
               v-model="desc_hab"
               id="desc_hab"
               :disabled="isDisabled"
               placeholder="Descrição de habilidade será gerada..."
-              class=" h-[100rem] bg-[#2A753D] p-4 focus:outline-none flex resize-none shadow-xl justify-start rounded-xl"
+              class="h-[10rem] bg-[#2A753D] p-4 focus:outline-none flex resize-none shadow-xl justify-start rounded-xl"
             >
             </textarea>
+            <div
+            v-else
+            class="h-[10rem] bg-[#2A753D] p-4 items-center justify-center focus:outline-none flex resize-none shadow-xl rounded-xl">
+            <Loader/>  
+          </div>
+            <div
+              v-if="Play"
+              @click="habilitarInput"
+              class="bg-[#FFD600] text-black p-[0.3rem] w-32 shadow-md rounded-xl flex justify-center"
+            >
+              <p v-if="isDisabled" class="font-bold text-base">Aprimorar</p>
+              <p v-else class="font-bold text-base">Voltar</p>
+            </div>
+
             <textarea
+              v-if="!loading"
               rows="4"
               v-model="desc_ati"
               id="desc_ati"
               :disabled="isDisabled"
               placeholder="Descrição de atitude será gerada..."
-              class=" h-[100rem] bg-[#2A753D] p-4 focus:outline-none flex resize-none shadow-xl justify-start rounded-xl"
+              class="h-[10rem] bg-[#2A753D] p-4 focus:outline-none flex resize-none shadow-xl justify-start rounded-xl"
             >
             </textarea>
+            <div
+            v-else
+            class="h-[10rem] bg-[#2A753D] p-4 focus:outline-none flex resize-none shadow-xl justify-center items-center rounded-xl">
+            <Loader/>  
+          </div>
+            <div
+              v-if="Play"
+              @click="habilitarInput"
+              class="bg-[#FFD600] text-black p-[0.3rem] w-32 shadow-md rounded-xl flex justify-center"
+            >
+              <p v-if="isDisabled" class="font-bold text-base">Aprimorar</p>
+              <p v-else class="font-bold text-base">Voltar</p>
+            </div>
             <textarea
+              v-if="!loading"
               rows="4"
               v-model="desc_cap"
               id="desc_cap"
               :disabled="isDisabled"
               placeholder="sua descrição será gerada"
-              class=" h-[100rem] text-xs xl:text-base bg-[#2A753D] p-4 focus:outline-none flex resize-none shadow-xl justify-start rounded-xl"
+              class="h-[10rem] text-xs xl:text-base bg-[#2A753D] p-4 focus:outline-none flex resize-none shadow-xl justify-start rounded-xl"
             >
             </textarea>
             <div
+            v-else
+            class="h-[10rem] bg-[#2A753D] items-center justify-center p-4 focus:outline-none flex resize-none shadow-xl rounded-xl">
+            <Loader/>   
+          </div>
+            <div
+              v-if="Play"
+              @click="habilitarInput"
+              class="bg-[#FFD600] text-black p-[0.3rem] w-32 shadow-md rounded-xl flex justify-center"
+            >
+              <p v-if="isDisabled" class="font-bold text-base">Aprimorar</p>
+              <p v-else class="font-bold text-base">Voltar</p>
+            </div>
+            <div
               @click="getResponseChatgpt(name, level)"
-              class="bg-black cursor-pointer absolute top-[13.7rem] right-[5rem] rounded-full shadow-md xl:p-5 p-2 flex justify-center xl:h-[3.7rem] h-[2rem]"
+              class="bg-black cursor-pointer absolute top-[1rem] right-12 xl:right-[5rem] rounded-full shadow-md xl:p-5 p-2 flex justify-center xl:h-[3.7rem] h-[2rem]"
             >
               <div class="xl:flex hidden">
                 <img src="/assets/play.svg" width="20" height="20" alt="" srcset="" />
@@ -148,25 +190,16 @@ const played = () => {
               </div>
             </div>
           </div>
-
-          <div
-            v-if="Play"
-            @click="habilitarInput"
-            class="bg-[#FFD600] text-black p-[0.3rem] w-32 shadow-md rounded-xl flex justify-center"
-          >
-            <p v-if="isDisabled" class="font-bold text-base">Aprimorar</p>
-            <p v-else class="font-bold text-base">Voltar</p>
-          </div>
         </div>
         <div class="w-full flex justify-center text-[#fff]">
           <button
             v-if="Play"
-            class="bg-[#263001] w-[15rem] rounded-xl"
+            class="bg-[#263001] w-[20rem] rounded-xl"
             @click="cadatrar"
             type="submit"
             value="Cadastrar"
           >
-            <p class="text-lg font-bold p-1">Cadastrar</p>
+            <p class="text-xl font-bold p-1">Cadastrar</p>
           </button>
         </div>
       </div>
