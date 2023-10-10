c1b604c6b6a3764dcefd2f38e6780ffc6c48b171
jumajubs <juliamaria215.com>
2023-10-10 08:12:55 -0300

    adding search filters and match colors

--- src/views/Ranqueamento.vue
+++ src/views/Ranqueamento.vue
@@ -1,17 +1,33 @@
 <script  setup>
     import OpenMenu from '@/components/menu/OpenMenu.vue'
 </script>
-<template >
-  <div >
-    <OpenMenu/>
- 
+<template>
+  <div>
+    <OpenMenu />
     <div>
       <h1 class="titulo">Ranqueamento de Candidatos</h1>
-      <div v-for="(candidato, index) in candidatos" :key="index" class="candidato-card">
+
+      <!-- Caixa de filtro por porcentagem de match -->
+      <div class="filtro">
+        <label for="filtroMatch">Selecionar porcentagem de match:</label>
+        <select id="filtroMatch" v-model="filtroSelecionado" @change="filtrarCandidatos" class="borda-filtro">
+          <option value="todos">Todas</option>
+          <option value="0-25">0% - 25%</option>
+          <option value="26-50">26% - 50%</option>
+          <option value="51-75">51% - 75%</option>
+          <option value="76-100">76% - 100%</option>
+        </select>
+      </div>
+
+      <!-- Lista de candidatos filtrados -->
+      <div v-for="(candidato, index) in candidatosFiltrados" :key="index" class="candidato-card">
         <div class="conteudo-candidatos">
           <span class="nome">{{ candidato.nome }}</span>
           <div class="barra">
-            <div class="barra-preenchida" :style="{ width: `${candidato.match}%` }"></div>
+            <div
+              class="barra-preenchida"
+              :style="{ width: `${candidato.match}%`, backgroundColor: getCor(candidato.match) }"
+            ></div>
           </div>
           <span class="match">{{ candidato.match }}%</span>
           <router-link :to="'/visualizar/' + candidato.id">
@@ -37,7 +53,38 @@ export default {
         { id: 7, nome: 'Candidato 7', match: 95 },
         { id: 8, nome: 'Candidato 8', match: 65 },
       ],
+      filtroSelecionado: '',
     };
+  },
+  computed: {
+    candidatosFiltrados() {
+      if (this.filtroSelecionado === '0-25') {
+        return this.candidatos.filter(candidato => candidato.match >= 0 && candidato.match <= 25);
+      } else if (this.filtroSelecionado === '26-50') {
+        return this.candidatos.filter(candidato => candidato.match >= 26 && candidato.match <= 50);
+      } else if (this.filtroSelecionado === '51-75') {
+        return this.candidatos.filter(candidato => candidato.match >= 51 && candidato.match <= 75);
+      } else if (this.filtroSelecionado === '76-100') {
+        return this.candidatos.filter(candidato => candidato.match >= 76 && candidato.match <= 100);
+      } else {
+        return this.candidatos;
+      }
+    },
+  },
+  methods: {
+    filtrarCandidatos() {},
+    getCor(match) {
+      // Definir cores com base na porcentagem de match
+      if (match >= 0 && match <= 25) {
+        return 'red';
+      } else if (match >= 26 && match <= 50) {
+        return 'orange';
+      } else if (match >= 51 && match <= 75) {
+        return 'yellow';
+      } else {
+        return 'green';
+      }
+    },
   },
 };
 </script>
@@ -48,6 +95,15 @@ export default {
   margin-bottom: 30px;
   font-weight: bold;
   font-size: 20px;
+}
+
+.borda-filtro {
+  border: 1px solid #000;
+}
+
+.filtro {
+  text-align: center;
+  margin-bottom: 20px;
 }
 
 .candidato-card {
@@ -74,18 +130,16 @@ export default {
 }
 
 .barra {
-  flex: 4;
+  width: 100%;
   background-color: #eee;
   height: 10px;
-  margin: 0 10px;
-  display: flex;
-  align-items: center;
+  margin-top: 8px;
 }
 
 .barra-preenchida {
-  flex: 1;
   height: 100%;
-  background-color: #007bff;
+  width: 0;
+  transition: width 0.5s ease;
 }
 
 .visualizar-button {
