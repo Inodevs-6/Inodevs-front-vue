6082a71a95e64d68501739f743132efd27596980
Kauã <kaua.reno@gmail.com>
2023-10-14 20:45:26 -0300

    Adapting the registration page according to customer confirmation

--- src/views/Ranqueamento.vue
+++ src/views/Ranqueamento.vue
@@ -1,46 +1,46 @@
-<script  setup>
-    import OpenMenu from '@/components/menu/OpenMenu.vue'
-    import LabelLista from '@/components/labels/Label-lista.vue';
-    import { defineProps, onMounted, ref } from 'vue';
-    import api from '@/services/api';
-    const erro = ref('')
-    const vaga = ref([])
-    const loading = ref(true)
-    const props = defineProps({
-      id: {
-        type: String,
-        required: true,
-      },
-    });
+<script setup>
+import OpenMenu from '@/components/menu/OpenMenu.vue'
+import LabelLista from '@/components/labels/Label-lista.vue'
+import { defineProps, onMounted, ref } from 'vue'
+import api from '@/services/api'
+const erro = ref('')
+const vaga = ref([])
+const loading = ref(true)
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  }
+})
 
 const fetchVagas = async () => {
-  loading.value = true 
-  try{
+  loading.value = true
+  try {
     const response = await api.get(`/vaga/match/${props.id}`)
-    vaga.value = response.data 
+    vaga.value = response.data
     console.log(repose.data)
-  }catch(error){
+  } catch (error) {
     if (error instanceof Error) {
-    erro.value = error.message;
-  } else {
-    erro.value = 'Ocorreu um erro desconhecido.';
-  }
+      erro.value = error.message
+    } else {
+      erro.value = 'Ocorreu um erro desconhecido.'
+    }
   }
   loading.value = false
 }
 
 const fetchCandidatos = async () => {
-  loading.value = true 
-  try{
+  loading.value = true
+  try {
     const response = await api.get(`/vaga/match/${props.id}`)
-    vaga.value = response.data 
+    vaga.value = response.data
     console.log(repose.data)
-  }catch(error){
+  } catch (error) {
     if (error instanceof Error) {
-    erro.value = error.message;
-  } else {
-    erro.value = 'Ocorreu um erro desconhecido.';
-  }
+      erro.value = error.message
+    } else {
+      erro.value = 'Ocorreu um erro desconhecido.'
+    }
   }
   loading.value = false
 }
@@ -48,50 +48,80 @@ onMounted(fetchVagas)
 onMounted(fetchCandidatos)
 </script>
 <template>
-  <div class="bg-white relative flex w-screen  overflow-x-hidden overflow-y-scroll h-screen">
+  <div class="bg-white relative flex w-screen overflow-x-hidden overflow-y-scroll h-screen">
     <OpenMenu />
     <div class="flex xl:ml-[5rem] w-screen items-center flex-col bg-white">
-      <h1 class="text-center font-medium xl:text-3xl text-xl xl:mt-7 mt-3 flex w-full h-10 justify-center items-center">
+      <h1
+        class="text-center font-medium xl:text-3xl text-xl xl:mt-7 mt-3 flex w-full h-10 justify-center items-center"
+      >
         Ranqueamento de {{ vaga.nome }}
       </h1>
 
-      
       <div class="filtro">
         <label for="minMatch">Mínimo Match (%):</label>
-        <input type="number" id="minMatch" v-model="minMatch" @input="filtrarCandidatos" class="borda-filtro" />
+        <input
+          type="number"
+          id="minMatch"
+          v-model="minMatch"
+          @input="filtrarCandidatos"
+          class="borda-filtro"
+        />
         <label for="maxMatch">Máximo Match (%):</label>
-        <input type="number" id="maxMatch" v-model="maxMatch" @input="filtrarCandidatos" class="borda-filtro" />
+        <input
+          type="number"
+          id="maxMatch"
+          v-model="maxMatch"
+          @input="filtrarCandidatos"
+          class="borda-filtro"
+        />
       </div>
-      <LabelLista name="Nome"  estilo="bg-[#FFD600] w-[7rem] absolute font-bold top-[9.4rem] left-[9rem] shadow-md rounded-lg text-center z-10"/>
-      <LabelLista name="Match" estilo="bg-[#FFD600] w-[7rem] absolute font-bold top-[9.4rem] right-[32rem] shadow-md rounded-lg text-center z-10"/>
-      <LabelLista name="Porcetagem" estilo="bg-[#FFD600] w-[7rem] absolute font-bold top-[9.4rem] right-[7rem] shadow-md rounded-lg text-center z-10"/>
-      <section class="xl:w-[88vw] h-[70vh] w-[90%] relative overflow-auto flex flex-col gap-4 p-4 pt-7 mt-[3rem] bg-[#1DEEA3] shadow-md bg-opacity-30 rounded-2xl">
-        <div v-for="(candidato, index) in candidatosFiltrados" :key="index" class="h-[3rem] text-white font-bold w-full flex-row flex justify-around items-center rounded-lg bg-[#2A753D]">
-        <div class="conteudo-candidatos flex items-center justify-around h-[3rem]">
-          <span class="nome w-[20%]  pl-4">{{ candidato.nome }}</span>
-          <div class="barra rounded-full shadow-md border-black">
-            <div
-              class="barra-preenchida shadow-md rounded-full  "
-              :style="{ width: `${candidato.match}%`, backgroundColor: getCor(candidato.match) }"
-            ></div>
-          </div>
-          <div class="w-[10%] flex justify-center items-center ">
-              <div class="match-circle  shadow-md" :style="{ backgroundColor: getCor(candidato.match) }">
-              <span class="match text-black">{{ candidato.match }}%</span>
+      <LabelLista
+        name="Nome"
+        estilo="bg-[#FFD600] w-[7rem] absolute font-bold top-[9.4rem] left-[9rem] shadow-md rounded-lg text-center z-10"
+      />
+      <LabelLista
+        name="Match"
+        estilo="bg-[#FFD600] w-[7rem] absolute font-bold top-[9.4rem] right-[32rem] shadow-md rounded-lg text-center z-10"
+      />
+      <LabelLista
+        name="Porcetagem"
+        estilo="bg-[#FFD600] w-[7rem] absolute font-bold top-[9.4rem] right-[7rem] shadow-md rounded-lg text-center z-10"
+      />
+      <section
+        class="xl:w-[88vw] h-[70vh] w-[90%] relative overflow-auto flex flex-col gap-4 p-4 pt-7 mt-[3rem] bg-[#1DEEA3] shadow-md bg-opacity-30 rounded-2xl"
+      >
+        <div
+          v-for="(candidato, index) in candidatosFiltrados"
+          :key="index"
+          class="h-[3rem] text-white font-bold w-full flex-row flex justify-around items-center rounded-lg bg-[#2A753D]"
+        >
+          <div class="conteudo-candidatos flex items-center justify-around h-[3rem]">
+            <span class="nome w-[20%] pl-4">{{ candidato.nome }}</span>
+            <div class="barra rounded-full shadow-md border-black">
+              <div
+                class="barra-preenchida shadow-md rounded-full"
+                :style="{ width: `${candidato.match}%`, backgroundColor: getCor(candidato.match) }"
+              ></div>
             </div>
+            <div class="w-[10%] flex justify-center items-center">
+              <div
+                class="match-circle shadow-md"
+                :style="{ backgroundColor: getCor(candidato.match) }"
+              >
+                <span class="match text-black">{{ candidato.match }}%</span>
+              </div>
             </div>
-          <!-- <router-link :to="'/visualizar/' + candidato.id">
+            <!-- <router-link :to="'/visualizar/' + candidato.id">
             <button class="visualizar-button">Visualizar</button>
           </router-link> -->
+          </div>
         </div>
-      </div>
       </section>
-    
     </div>
   </div>
 </template>
 
-<script >
+<script>
 export default {
   data() {
     return {
@@ -109,45 +139,44 @@ export default {
         { id: 11, nome: 'Candidato 11', match: 24 },
         { id: 12, nome: 'Candidato 12', match: 30 },
         { id: 13, nome: 'Candidato 13', match: 43 },
-        { id: 14, nome: 'Candidato 14', match: 50 },
+        { id: 14, nome: 'Candidato 14', match: 50 }
       ],
-      minMatch: '', 
-      maxMatch: '', 
-    };
+      minMatch: '',
+      maxMatch: ''
+    }
   },
   computed: {
     candidatosFiltrados() {
-      return this.candidatos.filter(candidato => {
+      return this.candidatos.filter((candidato) => {
         if (this.minMatch === '' && this.maxMatch === '') {
-          return true;
+          return true
         } else if (this.minMatch === '') {
-          return candidato.match <= parseInt(this.maxMatch);
+          return candidato.match <= parseInt(this.maxMatch)
         } else if (this.maxMatch === '') {
-          return candidato.match >= parseInt(this.minMatch);
+          return candidato.match >= parseInt(this.minMatch)
         } else {
           return (
             candidato.match >= parseInt(this.minMatch) && candidato.match <= parseInt(this.maxMatch)
-          );
-      }
-      });
-    },
+          )
+        }
+      })
+    }
   },
   methods: {
     filtrarCandidatos() {},
     getCor(match) {
-      
       if (match >= 0 && match <= 25) {
-        return 'red';
+        return 'red'
       } else if (match >= 26 && match <= 50) {
-        return 'orange';
+        return 'orange'
       } else if (match >= 51 && match <= 75) {
-        return 'yellow';
+        return 'yellow'
       } else {
-        return 'green';
+        return 'green'
       }
-    },
-  },
-};
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -187,7 +216,6 @@ export default {
   width: 100%;
 }
 
-
 .match {
   display: flex;
   text-align: center;
@@ -199,15 +227,13 @@ export default {
   width: 60%;
   background-color: #eee;
   height: 10px;
-  
-  
 }
 
 .barra-preenchida {
   height: 100%;
   width: 0;
   transition: width 0.5s ease;
-  border: solid 1px black
+  border: solid 1px black;
 }
 
 .match-circle {
@@ -234,5 +260,3 @@ export default {
   cursor: pointer;
 }
 </style>
-
-
