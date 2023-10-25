8729d0e8340ab2d88b8bd1d6cdf9eee9aa658131
jumajubs <juliamaria215.com>
2023-10-25 10:21:05 -0300

    Adding password reset fields

---
+++ src/views/Redefinir_Senha.vue
@@ -0,0 +1,163 @@
+<script setup lang="ts">
+
+import Alert from '@/components/alert/Alert.vue'
+import api from '../services/api'
+import { ref } from 'vue'
+const nome = ref('')
+const cnpj = ref('')
+const email = ref('')
+const senha = ref('')
+const senhaNovamente = ref('')
+const descricao = ref('')
+const erro = ref('')
+const isDisabled = ref(true)
+const isDone = ref(false)
+const playMatch = ref(true)
+
+const valid = ref(false)
+
+const save = ref(false)
+
+const salvar = () => {
+  erro.value = ''
+
+  if (
+    !nome.value ||
+    !cnpj.value ||
+    !email.value ||
+    !senha.value ||
+    !senhaNovamente.value ||
+    senha.value !== senhaNovamente.value
+  ) {
+    if (senha.value !== senhaNovamente.value) {
+      erro.value = 'As senhas não estão iguais'
+    }
+    valid.value = true
+    return
+  }
+
+  erro.value = ''
+  try {
+    
+    api.post('/empresa', {
+      nome: nome.value,
+      cnpj: cnpj.value,
+      email: email.value,
+      senha: senha.value,
+      descricao: descricao.value
+    })
+    save.value = true
+  } catch (error) {
+    erro.value = (error as Error).message
+  }
+}
+
+</script>
+
+<template>
+  <div class="bg-white flex w-screen overflow-x-hidden overflow-y-scroll h-screen">
+    <OpenMenu />
+    <div class="flex xl:ml-[5rem] w-screen items-center flex-col bg-white">
+      <div v-if="isDone">
+        <Alert variant="success" />
+      </div>
+      <h1
+        class="text-center font-medium xl:text-3xl text-xl xl:mt-7 mt-3 flex w-full h-10 justify-center items-center"
+      >
+        Redefinição de Senha
+      </h1>
+      <div
+        class="xl:w-[80vw] w-[90%] flex flex-col gap-8 p-4 mt-[3rem] bg-[#1DEEA3] shadow-md bg-opacity-30 rounded-2xl relative"
+      >
+        
+        <div class="flex flex-col gap-5 xl:gap-0 xl:flex-row justify-between">
+          <div class="w-full">
+            
+            <div class="w-[82%] flex flex-col relative top-[3rem] left-2">
+              <span
+                class="bg-[#FFD600] w-[7rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10"
+              >
+                Nova Senha
+              </span>
+              <input
+                v-model="senha"
+                id="senha"
+                placeholder="******"
+                class="bg-[#084808] w-full h-11 p-2 pt-2 shadow-md outline-none rounded-xl text-[#FFF] relative z-0"
+                :disabled="!isDisabled"
+              />
+            </div>
+            <div class="w-[82%] flex flex-col relative top-[5rem] left-2">
+              <span
+                class="bg-[#FFD600] w-[13rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10"
+              >
+                Confirme a Nova Senha
+              </span>
+              <input
+                v-model="senhaNovamente"
+                id="senhaNovamente"
+                placeholder="******"
+                class="bg-[#084808] w-full h-11 p-2 pt-2 shadow-md outline-none rounded-xl text-[#FFF] relative z-0"
+                :disabled="!isDisabled"
+              />
+            </div>
+          </div>
+        </div>
+        <p v-if="senha !== senhaNovamente" class="text-red-600 text-lg font-bold">
+          As senhas não estão iguais !
+        </p>
+        <div class="w-full flex justify-center">
+          <div v-if="playMatch" class="w-[60%] flex justify-center text-[#fff]">
+            <button
+              class="bg-[#263001] w-[10rem] rounded-xl"
+              @click="salvar"
+              type="submit"
+              value="Enviar para Busca"
+            >
+              <p class="text-lg font-bold p-1">Confirmar</p>
+            </button>
+          </div>
+        </div>
+        <div class="w-full flex justify-center">
+          <div v-if="playMatch" class="w-[60%] flex justify-center text-[#fff]">
+            <button
+              class="bg-[#263001] w-[10rem] rounded-xl"
+              @click="salvar"
+              type="submit"
+              value="Enviar para Busca"
+            >
+              <p class="text-lg font-bold p-1">Cadastrar Nova Senha</p>
+            </button>
+          </div>
+        </div>
+        <div class="fixed bottom-2 right-5">
+          <div
+            v-if="valid"
+            class="bg-[#cc0000] w-[25rem] rounded-xl border-solid border-white border-2 text-center"
+            type="submit"
+          >
+            <p class="text-[#fff] text-lg font-bold p-1">Preencha todos os campos!</p>
+          </div>
+        </div>
+        <div class="fixed bottom-2 right-5">
+          <div
+            v-if="save"
+            class="bg-[#2A753D] w-[25rem] rounded-xl border-solid border-white border-2 text-center"
+            type="submit"
+          >
+            <p class="text-[#fff] text-lg font-bold p-1">Empresa cadastrada com sucesso!</p>
+          </div>
+        </div>
+        <div class="fixed bottom-2 right-5">
+          <div
+            v-if="erro"
+            class="bg-[#cc0000] w-[25rem] rounded-xl border-solid border-white border-2 text-center"
+            type="submit"
+          >
+            <p class="text-[#fff] text-lg font-bold p-1">{{ erro }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
