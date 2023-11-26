9fd98c912805726a71f2c975e67ee358d4c70720
Richardrafael <richard.rafael.soares@gmail.com>
2023-11-26 13:46:39 -0300

    end Forgot password

--- src/views/EmailVerified.vue
+++ src/views/EmailVerified.vue
@@ -3,55 +3,86 @@ import Alert from '@/components/alert/Alert.vue'
 import api from '../services/api'
 import { ref } from 'vue'
 import router from '@/router'
-const senha = ref('')
 const email = ref('')
-const senhaNovamente = ref('')
 const erro = ref('')
+const errorCode = ref("")
+const modalOpened = ref(false)
+const codigo = ref("")
 const isDone = ref(false)
+const loadingCode = ref(false)
 const playMatch = ref(true)
-const loading = ref(false)
-const valid = ref(false)
+const reenviado = ref(false)
+const codeverifed = ref(false)
+const senha = ref('')
+const senhaNovamente = ref("")
 
-const save = ref(false)
-
-<<<<<<< HEAD
-const verificar = () => {
-=======
 const verificar = async () => {
-  erro.value = ''
-  loading.value = true
-  try {
-    router.push('/esqueceu')
-  } catch (error) {
-    erro.value = (error as Error).message
-    console.log(erro)
-  }
+  errorCode.value = ''
+  // reenviado.value = true
 
-  loading.value = false
+  try {
+    await api.patch('/empresa/redefinicao-senha/' + email.value)
+    sucesso()
+  } catch (error) {
+    console.log((error as Error).message)
+    errorCode.value = "Algo deu errado ao enviar o e-mail!"
+  }
 }
 
-const salvar = () => {
-  erro.value = ''
+// const redefinir = async () => {
+//   errorCode.value = ''
+// if (!senha.value || !senhaNovamente.value || senha.value !== senhaNovamente.value) {
+//   if (senha.value !== senhaNovamente.value) {
+//     errorCode.value = 'As senhas não estão iguais'
+//   }
+//   return
+// }
 
-  if (!senha.value || !senhaNovamente.value || senha.value !== senhaNovamente.value) {
-    if (senha.value !== senhaNovamente.value) {
-      erro.value = 'As senhas não estão iguais'
-    }
-    valid.value = true
-    return
-  }
+// errorCode.value = ''
+//   api.patch('empresa/redefinir-senha/', {
+//     senha: senha.value,
+//     // senhaNovamente: senhaNovamente.value
+//   }).then(response => {
+// console.log(response);
+// sucesso();
+// }).catch(error => {
+//   errorCode.value = (error as Error).message;
+// console.error("Erro:", error);
+// });
+// }
 
->>>>>>> f8992612ccf0dbdba79d7d5ebf7479e40695148b
-  erro.value = ''
-  if (
-    email.value
-  )
-  try {
-    api.patch('empresa/redefinicao-senha/'+ email.value)
-  } catch (error) {
-    erro.value = (error as Error).message
-    console.log(erro)
-  }
+// const verificaCode = async () => {
+//   errorCode.value = ''
+//   loadingCode.value = true
+
+//   try {
+//     const response = await api.post('/empresa/tfaverificar', {
+//       id: 3 , 
+//       tfaCodigo: codigo.value
+//     })
+//     codeverifed.value = true
+//   } catch (error) {
+//     console.log((error as Error).message)
+//     errorCode.value = "Código inválido!"
+//   }
+
+//   loadingCode.value = false
+// }
+
+async function sucesso() {
+
+modalOpened.value = true
+setTimeout(() => {
+  router.push('/home')
+}, 3000)
+}
+
+const closeModal = () => {
+  modalOpened.value = false
+  errorCode.value = ''
+  codigo.value = ''
+  reenviado.value = false
+  senha.value = ''
 }
 </script>
 
@@ -84,9 +115,14 @@ const salvar = () => {
         class="xl:w-[80vw] h-[30vw] w-[90%] flex flex-col gap-4 p-4 mt-[3rem] bg-[#1DEEA3] shadow-md bg-opacity-30 rounded-2xl relative"
       >
         <div class="flex flex-col justify-center pt-4 gap-6 items-center">
-          <div class="xl:w-[45%] w-full flex flex-col relative">
-            <span
-              class="bg-[#FFD600] w-[7rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10"
+          <div class="xl:w-[80%]  flex flex-col relative">
+            <span v-if="modalOpened"
+              class="bg-[#FFD600] w-[7rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center "
+            >
+              Email
+            </span>
+            <span v-else
+              class="bg-[#FFD600] w-[7rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10 "
             >
               Email
             </span>
@@ -119,6 +155,25 @@ const salvar = () => {
             type="submit"
           >
             <p class="text-[#fff] text-lg font-bold p-1">{{ erro }}</p>
+          </div>
+          
+        </div>
+
+        <div
+          v-if="modalOpened"
+          id="myModal"
+          class="modal fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-20"
+        >
+          <div
+            class="modal-content flex items-center gap-5 justify-center flex-col bg-white p-5 rounded shadow-md w-[30rem] relative"
+          >
+            <h2 class="text-xl font-bold">Email enviado com Sucesso</h2>
+            <span v-if="erro" class="text-red-700 font-semibold">{{ erro }}</span>
+            <img src="/assets/sucess.svg" alt="sucess" width="50" />
+            <div class="w-full flex justify-center items-center flex-col">
+              <h2 class="text-base font-bold">Você será redirecionado para pagina de Login</h2>
+              <h2 class="text-base font-bold">Em Instantes receberá o seu Codigo</h2>
+            </div>
           </div>
         </div>
       </div>
--- src/views/LoginRedefinicao.vue
+++ src/views/LoginRedefinicao.vue
@@ -3,7 +3,7 @@ import Alert from '@/components/alert/Alert.vue'
 import api from '../services/api'
 import { ref, defineProps } from 'vue'
 import { useAuth } from '@/stores/auth'
-import router from '@/router';
+import router from '@/router'
 
 const senha = ref('')
 const codigo = ref('')
@@ -14,23 +14,18 @@ const isDone = ref(false)
 const playMatch = ref(true)
 const errorCode = ref()
 const loadingCode = ref(false)
-const modalOpened = ref(false)
-const auth = useAuth()
-
+const modalOpened = ref(true)
+const reenviado = ref(false)
+const modalOpened1 = ref(false)
+const codeverifed = ref(false)
+const email = ref("")
 const valid = ref(false)
-
-const save = ref(false)
-
 const props = defineProps({
   id: {
     type: String,
     required: true
   }
 })
-
-const confirmarSenha = () => {
-  isDisabled.value = false
-}
 
 const salvar = () => {
   erro.value = ''
@@ -44,47 +39,70 @@ const salvar = () => {
   }
 
   erro.value = ''
-    api.patch('empresa/redefinir-senha/' + props.id, {
-      senha: senha.value,
+  api
+    .patch('empresa/redefinir-senha/' + props.id, {
+      senha: senha.value
       // senhaNovamente: senhaNovamente.value
-    }).then(response => {
-  console.log(response);
-  sucesso();
-}).catch(error => {
-  erro.value = (error as Error).message;
-  console.error("Erro:", error);
-});
-}
-async function sucesso() {
-
-modalOpened.value = true
-setTimeout(() => {
-  router.push('/home')
-}, 2000)
+    })
+    .then((response) => {
+      console.log(response)
+      sucesso()
+    })
+    .catch((error) => {
+      erro.value = (error as Error).message
+      console.error('Erro:', error)
+    })
 }
 
-const verificar = async () => {
+
+const verificaCode = async () => {
   errorCode.value = ''
   loadingCode.value = true
 
   try {
     const response = await api.post('/empresa/tfaverificar', {
-      codigo: codigo.value
+      id: props.id , 
+      tfaCodigo: codigo.value
     })
-
-    if (response.data.token) {
-      auth.setToken(response.data.token)
-      auth.setUser(response.data.empresa)
-    } else {
-      console.log('Token não recebido.')
-      errorCode.value = "Código inválido!"
-    }
+    codeverifed.value = true
   } catch (error) {
     console.log((error as Error).message)
     errorCode.value = "Código inválido!"
   }
 
   loadingCode.value = false
+}
+
+const habilarreenviar = () => {
+  reenviado.value = true
+}
+
+const reenviar = async () => {
+  errorCode.value = ''
+
+
+  try {
+    await api.patch('/empresa/redefinicao-senha/' + email.value)
+    reenviado.value = false
+  } catch (error) {
+    console.log((error as Error).message)
+    errorCode.value = "Algo deu errado ao enviar o e-mail!"
+  }
+}
+
+const closeModal = () => {
+  modalOpened.value = false
+  errorCode.value = ''
+  codigo.value = ''
+  reenviado.value = false
+  senha.value = ''
+}
+
+async function sucesso() {
+  modalOpened1.value = true
+  setTimeout(() => {
+    router.push('/home')
+  }, 3000)
 }
 </script>
 
@@ -105,119 +123,73 @@ const verificar = async () => {
           </router-link>
         </div>
       </div>
-      <h1
-        class="text-center font-medium xl:text-3xl text-xl flex w-full h-10 justify-center items-center"
-      >
-        Redefinição de Senha
-      </h1>
+     <div v-if="modalOpened" id="myModal" class="modal fixed inset-0 flex items-center justify-center bg-black bg-opacity-50  z-20">
+    <div class="modal-content bg-white p-10 rounded shadow-md  w-[80%] relative">
+      <span id="closeModalBtn" class="close absolute top-1 right-4 text-2xl cursor-pointer" @click="closeModal">&times;</span>
+      <h2 class="text-2xl font-bold mb-5">Verificação em duas etapas</h2>
+      <p class="mb-3">O código durará apenas 2 minutos.</p>
+      <span v-if="errorCode" class="text-red-700 font-semibold">{{ errorCode }}</span>
       <p v-if="senha !== senhaNovamente" class="text-red-600 text-lg font-bold">
         As senhas não estão iguais !
       </p>
-      <div
-        class="xl:w-[80vw] h-[30vw] w-[90%] flex flex-col gap-4 p-4 mt-[3rem] bg-[#1DEEA3] shadow-md bg-opacity-30 rounded-2xl relative"
-      >
-        <div class="flex flex-col justify-center pt-4 gap-6 items-center">
-          <div class="xl:w-[45%] w-full flex flex-col relative">
-            <span
-              class="bg-[#FFD600] w-[7rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10"
-            >
-              Codigo
-            </span>
-            <input
-              v-model="codigo"
-              id="codigo"
-              placeholder="codigo"
-              class="bg-[#084808] w-full h-11 p-2 pt-2 shadow-md outline-none rounded-xl text-[#FFF] relative z-0"
-              type="text"
-            />
-          </div>
-          <div v-if="codigo" class="xl:w-[45%] w-full flex flex-col relative">
-            <span
-              class="bg-[#FFD600] w-[7rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10"
-            >
-              Nova Senha
-            </span>
-            <input
-              v-model="senha"
-              id="senha"
-              placeholder="******"
-              class="bg-[#084808] w-full h-11 p-2 pt-2 shadow-md outline-none rounded-xl text-[#FFF] relative z-0"
-              :disabled="!isDisabled"
-              type="password"
-            />
-          </div>
-
-          <div v-if="codigo" class="xl:w-[45%] w-full flex flex-col relative">
-            <span
-              class="bg-[#FFD600] w-[13rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10"
-            >
-              Confirme a Nova Senha
-            </span>
-            <input
-              v-model="senhaNovamente"
-              id="senhaNovamente"
-              placeholder="******"
-              class="bg-[#084808] w-full h-11 p-2 pt-2 shadow-md outline-none rounded-xl text-[#FFF] relative z-0"
-              :disabled="!isDisabled"
-              type="password"
-            />
-          </div>
+      <span v-else>&nbsp;</span>
+      <div v-if="!reenviado" class="relative w-[100%] mb-12 mt-8">
+        <span class="bg-[#FFD600] w-[12rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10">Código de Verificação</span>
+        <input v-model="codigo" id="email" type="text" placeholder="Entre com o código de verificação" class="bg-[#ffffff] w-full h-11 p-4 pt-4 shadow-md outline-none border-2 rounded-xl text-[#000] relative z-0"/>
+      </div>
+      <div v-else class="relative w-[100%] mb-12 mt-8">
+        <span class="bg-[#FFD600] w-[12rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10">Email</span>
+        <input v-model="email" id="email" type="text" placeholder="Entre com o seu email" class="bg-[#ffffff] w-full h-11 p-4 pt-4 shadow-md outline-none border-2 rounded-xl text-[#000] relative z-0"/>
+      </div>
+      <div v-if="codeverifed" class="relative w-[100%] mb-12 mt-8">
+        <span class="bg-[#FFD600] w-[12rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10">Senha</span>
+        <input v-model="senhaNovamente" id="senha" type="password" placeholder="Entre com sua nova senha" class="bg-[#ffffff] w-full h-11 p-4 pt-4 shadow-md outline-none border-2 rounded-xl text-[#000] relative z-0"/>
+      </div>
+      <div v-if="codeverifed " class="relative w-[100%] mb-12 mt-8">
+        <span class="bg-[#FFD600] w-[12rem] absolute bottom-[2.1rem] left-4 font-semibold shadow-md rounded-lg text-center z-10">Senha Novamente</span>
+        <input v-model="senha" id="senha" type="password" placeholder="Entre com o código de verificação" class="bg-[#ffffff] w-full h-11 p-4 pt-4 shadow-md outline-none border-2 rounded-xl text-[#000] relative z-0"/>
+      </div>
+      <div class="flex items-center h-10">
+        <div v-if="!codeverifed && !reenviado" class="w-[30%]" > 
+          <p v-if="!reenviado" class="text-blue-600 cursor-pointer" @click="habilarreenviar">Habilitar reenviar Código</p>
+          <p v-else>Código reenviado!</p>
         </div>
-
-        <div class="w-full flex justify-center mt-10">
-          <div v-if="playMatch" class="w-[60%] flex justify-center text-[#fff]">
-            <button
-              class="bg-[#263001] w-[10rem] rounded-xl"
-              @click="confirmarSenha"
-              type="submit"
-              value="Confirmar"
-            >
-              <p class="text-lg font-bold p-1">Confirmar</p>
-            </button>
-          </div>
+        <div v-if="reenviado" class="w-[30%]" > 
+          <p class="text-blue-600 cursor-pointer" @click="reenviar">Reenviar Código</p>
         </div>
-        <div class="w-full flex justify-center">
-          <div class="w-[60%] flex justify-center text-[#fff]">
-            <button
-              class="bg-[#263001] w-[10rem] rounded-xl"
-              @click="salvar"
-              type="submit"
-              value="Enviar para Busca"
-              :disabled="isDisabled"
-              :class="{ 'opacity-50': isDisabled }"
-            >
-              <p class="text-lg font-bold p-1">Cadastrar Nova Senha</p>
-            </button>
-          </div>
+        <div v-if="codeverifed" class="flex-1 text-center"> 
+          <button  class="bg-[#273201] w-[10rem] rounded-xl" @click="salvar" type="submit">
+            <p v-if="!loadingCode" class="text-lg font-bold p-1 text-white">Redefinir</p>
+            <div v-else class="flex justify-center items-center m-1"><Loader /></div>
+          </button>
         </div>
-        <div class="fixed bottom-2 right-5">
-          <div
-            v-if="valid"
-            class="bg-[#cc0000] w-[25rem] rounded-xl border-solid border-white border-2 text-center"
-            type="submit"
-          >
-            <p class="text-[#fff] text-lg font-bold p-1">Preencha todos os campos!</p>
-          </div>
-        </div>
-        <div class="fixed bottom-2 right-5">
-          <div
-            v-if="save"
-            class="bg-[#2A753D] w-[25rem] rounded-xl border-solid border-white border-2 text-center"
-            type="submit"
-          >
-            <p class="text-[#fff] text-lg font-bold p-1">Nova senha cadastrada com sucesso!</p>
-          </div>
-        </div>
-        <div class="fixed bottom-2 right-5">
-          <div
-            v-if="erro"
-            class="bg-[#cc0000] w-[25rem] rounded-xl border-solid border-white border-2 text-center"
-            type="submit"
-          >
-            <p class="text-[#fff] text-lg font-bold p-1">{{ erro }}</p>
-          </div>
+        <div v-else class="flex-1 text-right"> 
+          <button  class="bg-[#273201] w-[10rem] rounded-xl" @click="verificaCode" type="submit">
+            <p v-if="!loadingCode" class="text-lg font-bold p-1 text-white">Veficar</p>
+            <div v-else class="flex justify-center items-center m-1"><Loader /></div>
+          </button>
         </div>
       </div>
+    </div>
+  </div>
+
+  <div
+          v-if="modalOpened1"
+          id="myModal"
+          class="modal fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-20"
+        >
+          <div
+            class="modal-content flex items-center gap-5 justify-center flex-col bg-white p-5 rounded shadow-md w-[30rem] relative"
+          >
+            <h2 class="text-xl font-bold">Senha redefinida com Sucesso</h2>
+            <span v-if="erro" class="text-red-700 font-semibold">{{ erro }}</span>
+            <img src="/assets/sucess.svg" alt="sucess" width="50" />
+            <div class="w-full flex justify-center items-center flex-col">
+              <h2 class="text-base font-bold">Você será redirecionado para pagina de Login</h2>
+              <h2 class="text-base font-bold">Sua nova senha já pode ser usada</h2>
+            </div>
+          </div>
+        </div>
     </div>
   </div>
 </template>
